aedba4fbbe3d6a27a9502552c521fd0e28a1d4cb
mbosselut <duchmol7@hotmail.com>
2019-11-06 13:36:34 +0100

    Setting up Team model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const logging = () => console.log(`Listening on : ${port} `);
 const db = require('./db');
+const Team = require('./teams/model');
+const logging = () => console.log(`Listening on : ${port} `);
+
 app.listen(port, logging);
